c9b806aa0cf5526fb8792425863c5b6a3d05d76b
Nithin Kumar M <nithinbhargav007@gmail.com>
2022-08-09 17:31:37 +0530

    deleted old file

--- disctionary.py
+++
@@ -1,8 +0,0 @@
-d = {"Name": "Nithin" , "Age":27, "Location":"Bangalore"}
-print(d.items())
-def print_dict():
-    for key,value in d.items():
-        print(f" key and value is {key} and {value}") 
-
-d["pin code"] = 562163
-print_dict()
