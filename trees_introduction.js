67f35eb2ad0b6f553126c9dc81684f3fa4ddb7c6
Nithin Kumar M <nithinbhargav007@gmail.com>
2022-08-10 12:23:19 +0530

    added js code for trees

---
+++ trees_introduction.js
@@ -0,0 +1,16 @@
+// In a perfectly balanced binary tree - we get number of nodes in power of 2
+//Level 0 : 2^0 =1;
+// Level 1 = 2 nodes
+// Level 2 = 4 nodes
+
+//log N will tell how many times we search / traverse the tree to find the required node
+
+// BINARY SEARCH TREE
+
+//When we traverse to the right --> Value always increase
+//When we traverse to the left  --> Value always decreases
+
+// Disadvantage of binary search tree
+// 1. If we keep adding element to the right, the time complexity increases to O(n)
+// 2. there are other algorithms or trees like AVL to balance the binary search tree
+
